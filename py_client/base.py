c050bb7a1c8ecd26addb49d09234391e73850ec2
realsanjeev <realsanjeev2@gmail.com>
2023-08-24 22:07:28 +0545

    Simplifying base file -> changed from basic.py

---
+++ py_client/base.py
@@ -0,0 +1,31 @@
+import requests
+
+# HTTP requests endpoint.
+simple_endpoint = "http://localhost:8000/api/"
+model_endpoint = "http://localhost:8000/api/product"
+
+def get_response(url_endpoint: str,
+                json: dict=None,
+                params: dict=None):
+    print("*"*45)
+    print(f"{'*'*4}url: {url_endpoint}{'*'*4}")
+    response = requests.get(url_endpoint, params=params, json=json)
+    status = response.status_code
+    page_source = response.text
+    try:
+        response_json = response.json()
+        print("Json response: ", response_json)
+    except requests.JSONDecodeError:
+        print("response header: ", response.headers)
+        print("response status: ", status)
+    response.close()
+    return None
+
+# simple endpoint of django
+get_response(simple_endpoint, 
+             json={"message": "hello world"},
+             params={"q": "for sending param?q=search in url"}
+             )
+
+# moodel endpoint. It is in api app
+get_response(model_endpoint)
