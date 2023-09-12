a28bc8646d6ffd0b9359a4565f87c9d06ec282a3
realsanjeev <realsanjeev2@gmail.com>
2023-09-13 00:24:48 +0545

    js-client extendended for refresh  token

--- js_client/main.js
+++ js_client/main.js
@@ -1,17 +1,56 @@
 const loginForm = document.getElementById("login-form");
-const contentContainer = document.getElementById("product-container")
+const contentContainer = document.getElementById("product-container");
+const searchForm =document.getElementById("search-form");
 const baseEndpoint = "http://localhost:8000/api";
 
 if (loginForm) {
     loginForm.addEventListener("submit", handleLogin)
 }
+if (searchForm) {
+    searchForm.addEventListener("submit", handleSearch)
+}
+function handleSearch(e) {
+    e.preventDefault();
+    const searchFormData = new FormData(searchForm);
+    const queryData = Object.fromEntries(searchForm);
+    const searchParams = new URLSearchParams(queryData);
+    const searchEndpoints = `${baseEndpoint}/search/?${searchParams}`
+    const accessToken = localStorage.getItem("access")
+    const headers = {
+        "Content-Type": "application/json"
+    }
+    if (accessToken) headers["Authorization"] = `Bearer ${accessToken}`
+    const options = {
+        method: "GET",
+        headers: headers
+    }
+    fetch(endpoint, options)
+    .then(res => res.json())
+    .then(data => {
+        const validData = isTokenValid(data);
+        if (validData && contentContainer) {
+            contentContainer.innerHTML = ""
+            if (data && data.hits) {
+                let htmlStr = "";
+                for (let result of data.hits) {
+                    htmlStr += `<li>${result.title}</li>`
+                }
+                contentContainer.innerHTML = htmlStr;
+                if (data.hits.length===0) {
+                    contentContainerinnerHTML = "<p>No results Found</p>"
+                } else {
+                    contentContainer.innerHTML = "<p>No results found</p>"
+                }
+            }
+        }
+    })
+    .catch(err => console.log("error search: ", err))
+}
 
-function handleLogin(e) {
-    console.log(e);
+function handleLogin (e) {
     e.preventDefault();
     const loginEndpoint = `${baseEndpoint}/token/`;
     const loginFormData = new FormData(loginForm);
-    console.log(loginFormData)
     const loginObjectFormData = Object.fromEntries(loginFormData);
     const options = {
         method: "POST",
@@ -30,10 +69,17 @@ function handleLogin(e) {
     })
 }
 
-const handleAuthData = (authData, callback) => {
-    localStorage.setItem("access", authData.access);
-    localStorage.setItem("refresh", authData.refresh)
-    if (callback) {
+function handleAuthData(authData, callback) {
+    const { access, refresh } = authData;
+
+    if (typeof access !== 'string' || typeof refresh !== 'string') {
+        return;
+    }
+
+    localStorage.setItem('access', access);
+    localStorage.setItem('refresh', refresh);
+
+    if (callback && typeof callback === 'function') {
         callback();
     }
 }
@@ -41,6 +87,7 @@ const handleAuthData = (authData, callback) => {
 const productList = () => {
     const productListEndpoint = `${baseEndpoint}/products`
     const accessToken = localStorage.getItem("access");
+    console.log(accessToken)
     const options = {
         method: "GET",
         headers: {
@@ -49,16 +96,103 @@ const productList = () => {
         }
     }
     fetch(productListEndpoint, options)
-    .then(res => res.json())
-    .then(data => {
-        writeInContainer(data);
+    .then(res => {
+        return Promise.all([res.json(), Promise.resolve(res.status)]);
     })
-    .catch(err => console.log("Error: ", err));
+    .then(([data, statusCode])=> {
+        if (statusCode !=200) return refreshAccessToken();
+        writeInContainer(data)
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+    });
 }
 
-const writeInContainer = (data) => {
+function writeInContainer(data) {
     if (contentContainer) {
         contentContainer.innerHTML = `<pre>${JSON.stringify(data, null, 4)}</pre>`
     }
 }
-productList();
+
+function isTokenValid (token){
+    if (token.code && token.code === "token_not_valid") {
+        alert("Please login again!!!");
+        return false
+    }
+    return true
+}
+
+function validateJWTToken() {
+    const endpoint = `${baseEndpoint}/token/verify/`
+    const accessToken = localStorage.getItem("access") || null;
+    if (!accessToken) {
+        refreshAccessToken();
+        return false;
+    }
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            token: localStorage.getItem("access")
+        })
+    }
+    fetch(endpoint, options)
+    .then(res => {
+        console.log("res: ",res)
+        return res.json()})
+    .then(data => {
+        // refresh token
+        console.log("validate res: ",data)
+    })
+    .catch(err => console.log("Validate error:", err))
+}
+
+function refreshAccessToken() {
+    const refreshToken = localStorage.getItem("refresh") || null;
+    const refreshTokenEndpoint = `${baseEndpoint}/token/refresh/`;
+
+    if (!refreshToken) {
+        // contentContainer.innerHTML = "<h3>Unauthorization</h3>";
+        return false;
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access")}`
+        },
+        body: JSON.stringify({refresh: refreshToken}) 
+    };
+
+    fetch(refreshTokenEndpoint, options)
+        .then(res => {
+            return Promise.all([res.json(), Promise.resolve(res.status)]);
+        })
+        .then(([data, statusCode]) => {
+            if (statusCode === 200) {
+                localStorage.setItem("access", data.access);
+                productList();
+            }
+            console.log(statusCode, "refresh res: ", data);
+        })
+        .catch(err => console.log("err: ", err));
+}
+
+
+function isPreviousAuthenticated() {
+    const accessToken = localStorage.getItem("access") || null;
+    const refreshToken = localStorage.getItem("refresh") || null;
+    if (!accessToken){
+        if (!refreshToken) {
+            // contentContainer.innerHTML = "<h3>Authentication needed</h3>"
+            return null
+        }
+    }
+    productList()
+}
+
+isPreviousAuthenticated();
